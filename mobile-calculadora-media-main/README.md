c91ac551910348cf69c5b47965a011265b12dc1f
Lipee <156234256+fellipenn9@users.noreply.github.com>
2024-01-11 17:25:28 -0300

    Add files via upload

---
+++ mobile-calculadora-media-main/README.md
@@ -0,0 +1,15 @@
+## 📚 Sobre
+Projeto de calculadora feito utilizando o Android Studio com a linguagem Java, com regras de cálculo de nota de acordo com o que a Universidade Veiga de Almeida utiliza na modalidade EaD.
+
+## 📜 Regras
+<img src="https://i.imgur.com/NccZtQZ.jpg" alt="Regras de cálculo" width="700">
+
+
+## 📸 Screenshots
+<img src="https://i.imgur.com/ZsTXprh.jpg" alt="Print de tela do aplicativo 1" height="700"><img src="https://i.imgur.com/lmZXtB0.jpg" alt="Print de tela do aplicativo 2" height="700">
+<img src="https://i.imgur.com/t38uyXe.jpg" alt="Print de tela do aplicativo 3" height="700"><img src="https://i.imgur.com/BmNu6ug.jpg" alt="Print de tela do aplicativo 4" height="700">
+<img src="https://i.imgur.com/r3U5Ixs.jpg" alt="Print de tela do aplicativo 5" height="700"><img src="https://i.imgur.com/xsgEWrF.jpg" alt="Print de tela do aplicativo 6" height="700">
+
+
+### ⚙ Versão da ferramenta
+Android Studio Arctic Fox | 2020.3.1 Patch 1
---
+++ mobile-calculadora-media-main/app/src/main/java/com/example/calculadoramedia/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.calculadoramedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText notaA1, notaA2, notaA3;
+    TextView resultado, aviso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        notaA1 = (EditText) findViewById(R.id.inputA1);
+        notaA2 = (EditText) findViewById(R.id.inputA2);
+        notaA3 = (EditText) findViewById(R.id.inputA3);
+        resultado = (TextView) findViewById(R.id.textResultado);
+        aviso = (TextView) findViewById(R.id.textAviso);
+    }
+    public void calcular(View v) {
+
+        String n1 = notaA1.getText().toString();
+        String n2 = notaA2.getText().toString();
+        String n3 = notaA3.getText().toString();
+
+        if(!n1.isEmpty() && !n2.isEmpty()) {
+            aviso.setText("");
+            float nota1 = Float.parseFloat(n1);
+            float nota2 = Float.parseFloat(n2);
+            float maiorNota = nota2;
+
+            if(!n3.isEmpty()) {
+                float nota3 = Float.parseFloat(n3);
+                if(nota3 > nota2) maiorNota = nota3;
+            }
+
+            if(nota1 < 0.1 || maiorNota < 5) {
+                double notaFinal = (nota1*0.4) + (maiorNota*0.6) / 2;
+                resultado.setText(String.format("%,.2f", notaFinal) + " - Reprovado");
+            } else {
+                double notaFinal = (nota1*0.4) + (maiorNota*0.6);
+
+                if(notaFinal >= 6) {
+                    resultado.setText(String.format("%,.2f", notaFinal) + " - Aprovado");
+                } else {
+                    resultado.setText(String.format("%,.2f", notaFinal) + " - Reprovado");
+                }
+            }
+        } else {
+            resultado.setText("");
+            aviso.setText("Os campos \"Nota da A1\" e \"Nota da \"A2\" são obrigatórios");
+        }
+    }
+
+    public void limpar(View v) {
+        notaA1.setText("");
+        notaA2.setText("");
+        notaA3.setText("");
+        resultado.setText("");
+        aviso.setText("");
+    }
+}
